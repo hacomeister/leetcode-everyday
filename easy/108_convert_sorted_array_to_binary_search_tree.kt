80fbced5e8d74493247eb666f44ae4a23338836d
hacomeister <erbashakann@gmail.com>
2021-03-29 23:38:44 +0300

    change #108 solution

--- easy/108_convert_sorted_array_to_binary_search_tree.kt
+++ easy/108_convert_sorted_array_to_binary_search_tree.kt
@@ -1,32 +1,32 @@
 //Leetcode #108 - Convert Sorted Array to Binary Search Tree Problem Solution in Kotlin
-//Runtime: 176 ms, faster than 86.29% of Kotlin online submissions for Convert Sorted Array to Binary Search Tree.
-//Memory Usage: 35.8 MB, less than 100.00% of Kotlin online submissions for Convert Sorted Array to Binary Search Tree.
+//Runtime: 172 ms, faster than 83.89% of Kotlin online submissions for Convert Sorted Array to Binary Search Tree.
+//Memory Usage: 35.8 MB, less than 94.63% of Kotlin online submissions for Convert Sorted Array to Binary Search Tree.
 
 fun main(args: Array<String>) {
-    val inputArray = intArrayOf(-10, -3, 0, 5, 9)
-    println(sortedArrayToBST(inputArray))
+    val prices = intArrayOf(-5, 1, 3, 4, 6, 10)
+    println("Result: " + sortedArrayToBST(prices))
 }
 
 fun sortedArrayToBST(nums: IntArray): TreeNode? {
 
     if (nums.size == 1) return TreeNode(nums[0])
 
-    return findChildrenByDivideAndConquer(nums, 0, nums.lastIndex)
+    return populateTree(nums, 0, nums.lastIndex)
 }
 
-fun findChildrenByDivideAndConquer(nums: IntArray, leftIndex: Int, rightIndex: Int): TreeNode? {
+fun populateTree(nums: IntArray, leftIndex: Int, rightIndex: Int): TreeNode? {
 
     if (leftIndex > rightIndex) return null
 
     val midIndex = (leftIndex + rightIndex) / 2
 
     return TreeNode(nums[midIndex]).apply {
-        left = findChildrenByDivideAndConquer(nums, leftIndex, midIndex - 1)
-        right = findChildrenByDivideAndConquer(nums, midIndex + 1, rightIndex)
+        left = populateTree(nums, leftIndex, midIndex - 1)
+        right = populateTree(nums, midIndex+1, rightIndex)
     }
 }
 
-class TreeNode(var `val`: Int) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
-}
+ class TreeNode(var `val`: Int) {
+         var left: TreeNode? = null
+         var right: TreeNode? = null
+     }
